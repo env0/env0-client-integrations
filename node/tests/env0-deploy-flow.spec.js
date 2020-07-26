79cc8acba8442ef36b1fe35548a34bd62f7639db
Yaron Yarimi <yaron.yarimi@env0.com>
2020-07-26 18:09:09 +0300

    fix tests

--- node/tests/env0-deploy-flow.spec.js
+++ node/tests/env0-deploy-flow.spec.js
@@ -3,33 +3,45 @@ const runDeployment = require("../env0-deploy-flow");
 
 jest.mock("../env0-deploy-utils");
 
+const mockRequiredOptions = {
+  projectId: 'proj0',
+  organizationId: 'org0',
+  apiKey: 'key0',
+  apiSecret: 'secret0'
+};
+
 describe("env0-deploy-flow", () => {
   const deployUtilsMock = DeployUtils.mock.instances[0];
-  describe("deploy", () => {
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('when there are missing required options', () => {
+    it('should fail with proper error message', () => {
+      expect(runDeployment('deploy', {})).rejects.toThrow(expect.stringContaining('Missing required options'));
+    })
+  });
+
+  describe("when all required options exist", () => {
     const environmentId = 12345;
 
-    beforeEach(() => {
-      jest.resetAllMocks();
-    });
+    describe('deploy', () => {
+      it("should fail when it fails to set configuration", async () => {
+        const configError = "Configuration error";
+        deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
+        deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("ACTIVE");
+        deployUtilsMock.setConfiguration.mockRejectedValue(new Error(configError));
 
-    it("should fail when it fails to set configuration", async () => {
-      const configError = "Configuration error";
-      deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
-      deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("ACTIVE");
-      deployUtilsMock.setConfiguration.mockRejectedValue(new Error(configError));
+        expect(runDeployment('deploy', mockRequiredOptions, { name: "shoes", value: "socks " })).rejects.toThrow(configError);
+      });
 
-      return expect(
-        runDeployment({ action: "deploy" }, [{ name: "shoes", value: "socks " }])
-      ).rejects.toThrow(configError);
-    });
+      it("should throw exception when environment status is FAILED", async () => {
+        deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
+        deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("FAILED");
 
-    it("should throw exception when environment status is FAILED", async () => {
-      deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
-      deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("FAILED");
-
-      return expect(runDeployment({ action: "deploy" })).rejects.toThrow(
-        `Environment ${environmentId} did not reach ACTIVE status`
-      );
-    });
+        expect(runDeployment('deploy')).rejects.toThrow(`Environment ${environmentId} did not reach ACTIVE status`);
+      });
+    })
   });
 });

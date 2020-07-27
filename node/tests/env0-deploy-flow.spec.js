d4b80b768c4657ef3c63133e0085fc8fee4f256d
Yaron Yarimi <yaron.yarimi@env0.com>
2020-07-27 17:50:02 +0300

    add tests for persistent options

--- node/tests/env0-deploy-flow.spec.js
+++ node/tests/env0-deploy-flow.spec.js
@@ -1,21 +1,40 @@
 const DeployUtils = require("../src/env0-deploy-utils");
 const runDeployment = require("../src/env0-deploy-flow");
+const persistedOptions = require('../src/commons/persisted-options');
+const { OPTIONS } = require('../src/commons/constants');
 
 jest.mock("../src/env0-deploy-utils");
+jest.mock('../src/commons/persisted-options');
+
+const { API_KEY, API_SECRET, ORGANIZATION_ID, PROJECT_ID } = OPTIONS;
 
 const mockRequiredOptions = {
-  projectId: 'proj0',
-  organizationId: 'org0',
-  apiKey: 'key0',
-  apiSecret: 'secret0'
+  [PROJECT_ID]: 'proj0',
+  [ORGANIZATION_ID]: 'org0',
+  [API_KEY]: 'key0',
+  [API_SECRET]: 'secret0'
 };
 
 describe("env0-deploy-flow", () => {
   const deployUtilsMock = DeployUtils.mock.instances[0];
+  const environmentId = 12345;
 
   beforeEach(() => {
     jest.resetAllMocks();
+    jest.spyOn(persistedOptions, 'read').mockReturnValue({});
   });
+
+  beforeEach(() => {
+    deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
+    deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("ACTIVE");
+  })
+
+  it('should read and write persisted options', async () => {
+    await runDeployment('deploy', mockRequiredOptions);
+
+    expect(persistedOptions.read).toBeCalled();
+    expect(persistedOptions.write).toBeCalled();
+  })
 
   describe('when there are missing required options', () => {
     it('should fail with proper error message', () => {
@@ -24,24 +43,28 @@ describe("env0-deploy-flow", () => {
   });
 
   describe("when all required options exist", () => {
-    const environmentId = 12345;
-
     describe('deploy', () => {
+
       it("should fail when it fails to set configuration", async () => {
         const configError = "Configuration error";
-        deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
-        deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("ACTIVE");
         deployUtilsMock.setConfiguration.mockRejectedValue(new Error(configError));
 
         expect(runDeployment('deploy', mockRequiredOptions, { name: "shoes", value: "socks " })).rejects.toThrow(configError);
       });
 
       it("should throw exception when environment status is FAILED", async () => {
-        deployUtilsMock.getEnvironment.mockResolvedValue({ id: environmentId });
         deployUtilsMock.pollEnvironmentStatus.mockResolvedValue("FAILED");
 
         expect(runDeployment('deploy')).rejects.toThrow(`Environment ${environmentId} did not reach ACTIVE status`);
       });
+
+      it('should use parameters with precedence over existing config', async () => {
+        jest.spyOn(persistedOptions, 'read').mockReturnValue({ [API_KEY]: 'key1', [API_SECRET]: 'secret1' });
+
+        await runDeployment('deploy', mockRequiredOptions);
+
+        expect(DeployUtils.init).toBeCalledWith(mockRequiredOptions);
+      })
     })
   });
 });

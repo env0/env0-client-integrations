7df03a3ff0cf09fb196fd84136cb000c59f3e091
Yaron Yarimi <yaron.yarimi@env0.com>
2020-07-28 21:48:13 +0300

    add tests

--- node/tests/env0-deploy-utils.spec.js
+++ node/tests/env0-deploy-utils.spec.js
@@ -1,27 +1,24 @@
-const DeployUtils = require("../src/env0-deploy-utils");
-const Env0ApiClient = require("../src/commons/api-client");
+const mockCallApi = jest.fn();
 
-jest.mock("../src/commons/api-client");
+// import needs to be after mock!
+const DeployUtils = require("../src/env0-deploy-utils");
+
+jest.mock("../src/commons/api-client", () =>
+    jest.fn().mockImplementation(() => ({ callApi: mockCallApi, sleep: () => Promise.resolve() }))
+);
+
+const mockEnvironmentId = 'environment0';
+const mockDeploymentId = 'deployment0';
 
 describe("env0-deploy-utils", () => {
-  let callApiMock;
   const deployUtils = new DeployUtils();
 
   beforeEach(() => {
-    callApiMock = Env0ApiClient.mock.instances[0].callApi;
-    callApiMock.mockResolvedValue([]);
-  })
-
-  describe("pollEnvironmentStatus", () => {
-    it("should return last environment status", async () => {
-      const mockStatus = "FAILED";
-      Env0ApiClient.mock.instances[0].callApi.mockResolvedValue({ status: mockStatus });
-      const returnedStatus = await deployUtils.pollEnvironmentStatus(12345);
-      expect(returnedStatus).toEqual(mockStatus);
-    });
+    jest.clearAllMocks();
+    mockCallApi.mockResolvedValue([]);
   });
 
-  describe('Setting Configuration', () => {
+  describe('set configuration', () => {
     it('should query existing configurations for update', async () => {
       await deployUtils.setConfiguration(
           {id:'environment-1', organizationId: 'organization-1'},
@@ -31,7 +28,7 @@ describe("env0-deploy-utils", () => {
           false
       );
 
-      expect(callApiMock).toHaveBeenCalledWith(
+      expect(mockCallApi).toHaveBeenCalledWith(
           'get',
           'configuration',
           {params: {blueprintId: 'blueprint-1', environmentId: 'environment-1', organizationId: 'organization-1'}}
@@ -47,7 +44,7 @@ describe("env0-deploy-utils", () => {
           false
       );
 
-      expect(callApiMock).toHaveBeenCalledWith(
+      expect(mockCallApi).toHaveBeenCalledWith(
           'post',
           'configuration',
           {
@@ -78,11 +75,107 @@ describe("env0-deploy-utils", () => {
           isSensitive
       );
 
-      expect(callApiMock).toHaveBeenCalledWith(
+      expect(mockCallApi).toHaveBeenCalledWith(
           'post',
           'configuration',
           { data: expect.objectContaining({ isSensitive } ) }
       );
     })
+  });
+
+  describe('wait for environment', () => {
+    it.each`
+    status
+    ${'CREATED'}
+    ${'INACTIVE'}
+    ${'ACTIVE'}
+    ${'FAILED'}
+    ${'TIMEOUT'}
+    `('should call api client once', async ({ status }) => {
+      mockCallApi.mockResolvedValue({ status });
+
+      await deployUtils.waitForEnvironment(mockEnvironmentId);
+
+      expect(mockCallApi).toBeCalledWith('get', `environments/${mockEnvironmentId}`,);
+      expect(mockCallApi).toBeCalledTimes(1);
+    })
+
+    it('should call api client twice', async () => {
+      mockCallApi.mockResolvedValueOnce({ status: 'TEST' });
+      mockCallApi.mockResolvedValueOnce({ status: 'ACTIVE' });
+
+      await deployUtils.waitForEnvironment(mockEnvironmentId);
+
+      expect(mockCallApi).toBeCalledWith('get', `environments/${mockEnvironmentId}`,);
+      expect(mockCallApi).toBeCalledTimes(2);
+    })
+  });
+
+  describe('poll deployment status', () => {
+    it('should call api twice', async () => {
+      // one for polling and one for writing the logs
+      mockCallApi.mockResolvedValueOnce({ status: 'SUCCESS' });
+
+      await deployUtils.pollDeploymentStatus(mockDeploymentId);
+
+      expect(mockCallApi).toBeCalledWith('get', `environments/deployments/${mockDeploymentId}`);
+      expect(mockCallApi).toBeCalledTimes(2);
+    });
+  });
+
+  describe('fetch deployment steps', () => {
+    const mockStep = { name: 'git:clone', status: 'SUCCESS' };
+
+    it('should call api', async () => {
+      mockCallApi.mockResolvedValueOnce([]);
+
+      await deployUtils.fetchDeploymentSteps(mockDeploymentId, []);
+
+      expect(mockCallApi).toBeCalledWith('get', `deployments/${mockDeploymentId}/steps`);
+    })
+
+    it.each`
+    stepsFromApi | stepsToSkip
+    ${[ { name: 'git:clone', status: 'SUCCESS' } ]} | ${[ 'git:clone' ]}
+    ${[{ name: 'git:clone', status: 'IN_PROGRESS' } ]} | ${[]}
+    `('should skip step log', async ({ stepsFromApi, stepsToSkip }) => {
+      mockCallApi.mockResolvedValueOnce(stepsFromApi);
+
+      const doneSteps = await deployUtils.fetchDeploymentSteps(mockDeploymentId, stepsToSkip);
+
+      expect(doneSteps).toEqual([]);
+    })
+
+    it('should get step log', async () => {
+      mockCallApi.mockResolvedValueOnce([ mockStep ]);
+      mockCallApi.mockResolvedValueOnce({ events: [] }); // mock for the write deployment step log function
+
+      const doneSteps = await deployUtils.fetchDeploymentSteps(mockDeploymentId, []);
+
+      expect(doneSteps).toEqual([ mockStep.name ])
+    })
+  })
+
+  describe('write deployment step log', () => {
+    const mockStepName = 'git:clone';
+
+    it('should call api once when hasMoreLogs=false', async () => {
+      mockCallApi.mockResolvedValueOnce({ events: [], hasMoreLogs: false });
+
+      await deployUtils.writeDeploymentStepLog(mockDeploymentId, mockStepName)
+
+      expect(mockCallApi).toBeCalledWith('get', `deployments/${mockDeploymentId}/steps/${mockStepName}/log`);
+      expect(mockCallApi).toBeCalledTimes(1);
+    });
+
+    it('should call api twice when hasMoreLogs=true', async () => {
+      mockCallApi.mockResolvedValueOnce({ events: [], hasMoreLogs: true });
+      mockCallApi.mockResolvedValueOnce({ events: [], hasMoreLogs: false });
+
+      await deployUtils.writeDeploymentStepLog(mockDeploymentId, mockStepName)
+
+      expect(mockCallApi).toBeCalledWith('get', `deployments/${mockDeploymentId}/steps/${mockStepName}/log`);
+      expect(mockCallApi).toBeCalledTimes(2);
+    });
   });
 });

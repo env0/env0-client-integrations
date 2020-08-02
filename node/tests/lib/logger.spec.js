66bd48c4f5c6f115da363b2a8e9bddc7238ed833
Yaron Yarimi <yaron.yarimi@env0.com>
2020-08-02 18:05:01 +0300

    add tests

---
+++ node/tests/lib/logger.spec.js
@@ -0,0 +1,37 @@
+const logger = require('../../src/lib/logger');
+const { options } = require('../../src/config/constants');
+
+global.console = {
+  log: jest.fn()
+};
+
+const mockSecret = 'AAAAABBBBBCCCCCDDDDD';
+const secureMockSecret = '***************DDDDD';
+
+describe('logger', () => {
+  it.each`
+    option
+    ${options.API_KEY}
+    ${options.API_SECRET}
+  `('should hide $option', ({ option }) => {
+    logger.setSecrets({ [option]: mockSecret });
+
+    logger.info(`${option}: ${mockSecret}`);
+
+    expect(console.log).toBeCalledWith(`${option}: ${secureMockSecret}`);
+  });
+
+  it.each`
+    option
+    ${options.ENVIRONMENT_NAME}
+    ${options.PROJECT_ID}
+    ${options.ORGANIZATION_ID}
+    ${options.BLUEPRINT_ID}
+  `('should not hide $option', ({ option }) => {
+    logger.setSecrets({ [option]: mockSecret });
+
+    logger.info(`${option}: ${mockSecret}`);
+
+    expect(console.log).toBeCalledWith(`${option}: ${mockSecret}`);
+  });
+});

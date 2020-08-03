6120744a8fb2507557102d001f78e6d1369acb5e
Yaron Yarimi <yaron.yarimi@env0.com>
2020-08-03 12:42:54 +0300

    update requires approval before destroy

--- node/src/commands/destroy.js
+++ node/src/commands/destroy.js
@@ -1,20 +1,32 @@
 const DeployUtils = require('../lib/deploy-utils');
+const { options } = require('../config/constants');
+const _ = require('lodash');
+
+const { PROJECT_ID, ENVIRONMENT_NAME, REQUIRES_APPROVAL, ARCHIVE_AFTER_DESTROY } = options;
+
+const assertEnvironmentExists = environment => {
+  if (!environment) {
+    throw new Error(`Could not find an environment with the name ${options[ENVIRONMENT_NAME]}`);
+  }
+};
 
 const destroy = async options => {
   const deployUtils = new DeployUtils();
 
   console.log('Waiting for deployment to start...');
-  const environment = await deployUtils.getEnvironment(options.environmentName, options.projectId);
+  const environment = await deployUtils.getEnvironment(options[ENVIRONMENT_NAME], options[PROJECT_ID]);
   let status;
 
-  if (environment) {
-    const deployment = await deployUtils.destroyEnvironment(environment);
-    status = await deployUtils.pollDeploymentStatus(deployment.id);
+  assertEnvironmentExists(environment);
 
-    if (options.archiveAfterDestroy) await deployUtils.archiveIfInactive(environment.id);
-  } else {
-    throw new Error(`Could not find an environment with the name ${options.environmentName}`);
+  if (!_.isUndefined(options[REQUIRES_APPROVAL]) && options[REQUIRES_APPROVAL] !== environment.requiresApproval) {
+    await deployUtils.updateEnvironment(environment, { requiresApproval: options[REQUIRES_APPROVAL] });
   }
+
+  const deployment = await deployUtils.destroyEnvironment(environment);
+  status = await deployUtils.pollDeploymentStatus(deployment.id);
+
+  if (options[ARCHIVE_AFTER_DESTROY]) await deployUtils.archiveIfInactive(environment.id);
 
   deployUtils.assertDeploymentStatus(status);
 };

11d7cafe9d4718222d1c67769554d1f054df8cc3
Yaron Yarimi <yaron.yarimi@env0.com>
2020-08-19 13:18:37 +0300

    add remove empty values util

--- node/src/lib/genetal-utils.js
+++ node/src/lib/genetal-utils.js
@@ -1,8 +1,17 @@
-const convertRequiresApprovalToBoolean = str => {
+const convertStringToBoolean = str => {
   if (str === 'true') return true;
   if (str === 'false') return false;
 };
 
+const removeEmptyValuesFromObj = payload =>
+  JSON.parse(
+    JSON.stringify(payload, (_, value) => {
+      if (value === undefined) return undefined;
+      return value;
+    })
+  );
+
 module.exports = {
-  convertRequiresApprovalToBoolean
+  convertStringToBoolean,
+  removeEmptyValuesFromObj
 };

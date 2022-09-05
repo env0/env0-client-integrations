a9caef236ebbfd508a88264bf51b981401c64e75
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2022-09-05 13:57:22 +0300

    Obtain JWT on client init (#112)

--- node/src/lib/api-client.js
+++ node/src/lib/api-client.js
@@ -3,13 +3,21 @@ const { version } = require('../../package.json');
 
 class Env0ApiClient {
   async init(apiKey, apiSecret) {
-    this.apiClient = axios.create({
-      baseURL: process.env.ENV0_API_URL || 'https://api.env0.com',
+    const baseURL = process.env.ENV0_API_URL || 'https://api.env0.com';
+
+    const { data: jwt } = await axios.get('auth/token?encoded=true', {
+      baseURL,
+      method: 'GET',
       auth: {
         username: apiKey,
         password: apiSecret
-      },
+      }
+    });
+
+    this.apiClient = axios.create({
+      baseURL: process.env.ENV0_API_URL || 'https://api.env0.com',
       headers: {
+        Authorization: `Bearer ${jwt}`,
         Accept: 'application/json',
         'Content-Type': 'application/json',
         'User-Agent': `env0-node-cli-${version}`

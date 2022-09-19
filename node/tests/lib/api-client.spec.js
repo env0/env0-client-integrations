89926213fb9f093b24e1b6f79f2eea7f45fe8733
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2022-09-19 19:11:12 +0300

    Revert "Obtain JWT on client init (#112)"
    This reverts commit a9caef236ebbfd508a88264bf51b981401c64e75.

--- node/tests/lib/api-client.spec.js
+++ node/tests/lib/api-client.spec.js
@@ -7,19 +7,12 @@ const mockKey = 'key0';
 const mockSecret = 'secret0';
 
 describe('api-client', () => {
-  const jwt = 'iamjwt';
-
   beforeEach(() => {
-    (axios.get).mockResolvedValue({ data: jwt });
     new ApiClient().init(mockKey, mockSecret);
   });
 
-  it('should obtain a jwt', () => {
-    expect(axios.get).toBeCalledWith('auth/token?encoded=true', expect.objectContaining({ auth: { username: mockKey, password: mockSecret } }));
-  });
-
-  it('should set authorization to jwt', () => {
-    expect(axios.create).toBeCalledWith(expect.objectContaining({ headers: expect.objectContaining({ Authorization: `Bearer ${jwt}` }) }));
+  it('should set authorization header', () => {
+    expect(axios.create).toBeCalledWith(expect.objectContaining({ auth: { username: mockKey, password: mockSecret } }));
   });
 
   it('should set user agent header', () => {
